5796b2e8611d861328b1a24b4f4c9bf2a49c9880
Asim Sudheer <asimsudheer512@gmail.com>
2025-12-23 18:08:51 +0530

    next update

--- base calorie calculator app/calorie calc.py
+++ base calorie calculator app/calorie calc.py
@@ -1,8 +1,16 @@
+food_calories = {
+    "rice": 130,
+    "egg": 155,
+    "apple": 52,
+    "banana": 89,
+    "bread": 265
+}
+
 
 calories = []
 
-def add_food(food,grams):
-    calorie = (food/100)*grams
+def add_food(food_name,grams):
+    calorie = (food_name/100)*grams
     return calorie
 
 def total_food():
@@ -12,16 +20,26 @@ def total_food():
 while True:
     print("--------------------------------------------------")
     print("1.Add food\n2.View Total calorie\n3.Exit")
-    choice = int(input("Enter your choice:"))
+    try:
+        choice = int(input("Enter your choice:"))
+    except ValueError:
+        print("Enter a valid number")
     if choice == 1:
         print("-----ADD FOOD-----")
-        food = input("Enter the name of the food:")
-        grams = int(input("Enter the amount of food:"))
-        calorie = add_food(food,grams)
+        food = input("Enter the name of the food:").lower()
+        try:
+            grams = int(input("Enter the amount of food:"))
+        except ValueError:
+            print("Enter the valid number")
+        food_name = food_calories[food]
+        if food_name is None:
+            print("The entered food details is not available")
+            continue
+        calorie = add_food(food_name,grams)
         calories.append(calorie)
         print("The food has been added")
     elif choice == 2:
         print("----TOTAL----")
         total_food()
     elif choice == 3:
-        break
+        break   

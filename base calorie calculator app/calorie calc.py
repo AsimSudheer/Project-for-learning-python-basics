dc182f62d4077e0db3d3e7fa77ef4450be198716
Asim Sudheer <asimsudheer512@gmail.com>
2025-12-23 18:07:02 +0530

    bit updated version

--- base calorie calculator app/calorie calc.py
+++ base calorie calculator app/calorie calc.py
@@ -1,13 +1,27 @@
-foods_calorie = {
-    "rice":200,
-    "egg":250,
-    "bacon":350
-}
 
-food = input("Enter the name of the food:")
-grams = int(input("Enter the grams:"))
-try:
-    calorie = (foods_calorie[food]/100)*grams
-    print("The calorie=",calorie)
-except:
-    print("The entered food does not contain in the database")
+calories = []
+
+def add_food(food,grams):
+    calorie = (food/100)*grams
+    return calorie
+
+def total_food():
+    print("Total calories in a day:",sum(calories))
+
+
+while True:
+    print("--------------------------------------------------")
+    print("1.Add food\n2.View Total calorie\n3.Exit")
+    choice = int(input("Enter your choice:"))
+    if choice == 1:
+        print("-----ADD FOOD-----")
+        food = input("Enter the name of the food:")
+        grams = int(input("Enter the amount of food:"))
+        calorie = add_food(food,grams)
+        calories.append(calorie)
+        print("The food has been added")
+    elif choice == 2:
+        print("----TOTAL----")
+        total_food()
+    elif choice == 3:
+        break
